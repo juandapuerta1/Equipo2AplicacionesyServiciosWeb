ad77b90ce5a1cc7878fd38cb77baa39e20f624a4
Alexmick12 <gogetassd1@gmail.com>
2026-09-05 00:29:07 -0500

    Se agrego games

--- app/main.py
+++ app/main.py
@@ -1,11 +1,6 @@
 from fastapi import FastAPI
-from app.core.database import Base, engine
-from app.routers import citas, games
-
-# 🔴 Comenta esta línea temporalmente
-# Base.metadata.create_all(bind=engine)
+from app.routers import games
 
 app = FastAPI(title="API Tienda Videojuegos")
 
-app.include_router(citas.router)
 app.include_router(games.router)
