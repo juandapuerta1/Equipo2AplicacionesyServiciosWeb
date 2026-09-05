28b4cbcb3bbe7c04d497cfc5a8ca325920d4b794
JuanGonz06 <juangonzalez1129675@correo.itm.edu.co>
2026-09-05 00:48:02 -0500

    feat: registrar endpoints de Mascota y router en main

--- app/main.py
+++ app/main.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
 from app.core.database import Base, engine
-from app.routers import citas
+from app.routers import citas, mascota
 
 # Esto crea la tabla en Neon automáticamente si no existe
 Base.metadata.create_all(bind=engine)
 
-app = FastAPI(title="API Agendamiento de Uñas")
+app = FastAPI(title="API Agendamiento de uñas y mascotas")
 
 # Registrando los endpoints del CRUD
 app.include_router(citas.router)
+app.include_router(mascota.router)
