6b1a5e63989e86b7e2cc68c5a077a39fa2aa6b27
JuanGonz06 <juangonzalez1129675@correo.itm.edu.co>
2026-09-05 01:21:48 -0500

    fix: quitar referencia a mascota en main.py

--- app/main.py
+++ app/main.py
@@ -1,12 +1,11 @@
 from fastapi import FastAPI
 from app.core.database import Base, engine
-from app.routers import citas, mascota
+from app.routers import citas
 
 # Esto crea la tabla en Neon automáticamente si no existe
 Base.metadata.create_all(bind=engine)
 
-app = FastAPI(title="API Agendamiento de uñas y mascotas")
+app = FastAPI(title="API Agendamiento de uñas")
 
 # Registrando los endpoints del CRUD
 app.include_router(citas.router)
-app.include_router(mascota.router)
